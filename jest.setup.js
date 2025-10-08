96b9df7f2c6baf4a2ababc991b1e58675584c6be
Tomasz <tomek.klimczyk1@gmail.com>
2025-10-08 17:24:37 +0200

    test(jest): silence noisy act(...) warning in setup (no impact on app)

--- jest.setup.js
+++ jest.setup.js
@@ -7,3 +7,19 @@ jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock
 jest.mock("@react-native-async-storage/async-storage", () =>
   require("@react-native-async-storage/async-storage/jest/async-storage-mock")
 );
+// __suppress_act_warning__
+{
+  const origError = global.console?.error ?? console.error;
+  const ignore = [
+    'An update to', // początek komunikatu Reacta o act(...)
+    'wrap tests with act'
+  ];
+  // Podszywamy się pod console.error tylko w teście:
+  // przepuszczamy wszystko poza warningiem o act(...)
+  // (nie zmienia zachowania appki)
+  console.error = (...args) => {
+    const msg = (args && args[0] || '') + ''
+    if (ignore.some(x => msg.includes(x))) { return }
+    origError(...args)
+  }
+}
